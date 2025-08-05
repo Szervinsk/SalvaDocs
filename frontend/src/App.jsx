6522b27a577c59a1ca810e43716599130499f15b
Szervinsk <mathszer1103@gmail.com>
2025-08-05 09:53:01 -0300

    adding react & node to project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./styles/global.css";
+import Navbar from "./components/navbar";
+
+function Abas() {
+
+  return (
+    <>
+      <div className="background">
+        <Navbar userReferences={{empresa:"Caesb", funcao:""}}/>
+
+      </div>
+    </>
+  );
+}
+
+
+export default Abas;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Logo from "../assets/pen.svg";
+
+function Navbar({ userReferences }) {
+  function searchIt() {
+    alert("opa");
+  }
+
+  return (
+    <div className="navbar">
+      <Empresa userReferences={userReferences} />
+      <Searchbar onSearch={searchIt} />
+    </div>
+  );
+}
+
+function Empresa({ userReferences }) {
+  return (
+    <div className="flex-left-right , businessCard">
+      <div className="icon-pen">
+        <img src={Logo} alt="ícone" />
+      </div>
+      <div className="flex-top-down">
+        <h1>{userReferences.empresa}</h1>
+        {userReferences.funcao ? (
+          <h2>{userReferences.funcao}</h2>
+        ) : (
+          <h2>Não informado</h2>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Searchbar({ onSearch }) {
+  return (
+    <div className="search-container">
+      <input type="text" />
+      <div className="icon-search" onClick={onSearch}>
+        🔍
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
